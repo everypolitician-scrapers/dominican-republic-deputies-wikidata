cabab1a8c2a1189d62c672dc73cc2c74fb7b754b
Tony Bowden <tony@tmtm.com>
2016-03-05 14:52:16 +0000

    Lots more people

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,13 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Chamber_of_Deputies_of_the_Dominican_Republic',
   after: '//h2/span[@id="Current_composition"]',
   before: '//h2/span[@id="Party_strengths_in_the_Chamber_of_Deputies"]',
   xpath: '//table//tr//td[1]//a[not(@class="new")]/@title',
 ) 
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+es_names = WikiData::Category.new( 'Categoría:Políticos de República Dominicana', 'es').member_titles
+  
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, es: es_names }, output: false)
